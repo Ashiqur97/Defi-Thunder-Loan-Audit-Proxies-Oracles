e838db8127ed16e74dcc48bce8de8908b4e3603a
Ashiqur Rahman <98506625+Ashiqur97@users.noreply.github.com>
2024-01-24 00:21:52 +0600

    recon

--- src/interfaces/IFlashLoanReceiver.sol
+++ src/interfaces/IFlashLoanReceiver.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity 0.8.20;
 
+//@audit unsed interface
+// it's a bad practice to edit love code for tests/mocks, we must remove this interface
 import { IThunderLoan } from "./IThunderLoan.sol";
 
 /**
@@ -9,6 +11,9 @@ import { IThunderLoan } from "./IThunderLoan.sol";
  */
 interface IFlashLoanReceiver {
     function executeOperation(
+        // q is the token, the token that's being borrowed
+        // @audit where the natspec?
+        // q amount is the amount of tokens that's being borrowed
         address token,
         uint256 amount,
         uint256 fee,
