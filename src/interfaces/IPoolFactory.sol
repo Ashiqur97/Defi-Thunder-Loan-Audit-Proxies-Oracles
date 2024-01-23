826a2a8b0ad694cbf1793954788ec832ff7fb119
Ashiqur Rahman <98506625+Ashiqur97@users.noreply.github.com>
2024-01-24 00:02:34 +0600

    recon ipoolfactory

--- src/interfaces/IPoolFactory.sol
+++ src/interfaces/IPoolFactory.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.20;
 
+// e this is probably the interface for the poolfactory.sol from tswap
+// why are we using tswap
 interface IPoolFactory {
     function getPool(address tokenAddress) external view returns (address);
 }
